be15ea9c595237acac0eaa26d0c1896988ba974c
Keisuke Takagi <mit@takagi.app>
2023-03-30 02:50:33 +0900

    feat: register ConfigImpl

--- src/core/service/di/serviceModule.ts
+++ src/core/service/di/serviceModule.ts
@@ -1,9 +1,14 @@
 import { container } from "tsyringe";
+import { Config } from "../config/Config";
+import { ConfigImpl } from "../config/impl/ConfigImpl";
 import { GlobalState } from "../globalState/GlobalState";
 import { GlobalStateImpl } from "../globalState/impl/GlobalStateImpl";
 
 export function serviceModule() {
   container.register<GlobalState>("GlobalState", {
     useClass: GlobalStateImpl
+  });
+  container.register<Config>("Config", {
+    useClass: ConfigImpl,
   });
 }
